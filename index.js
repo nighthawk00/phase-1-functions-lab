8b40505bae290d1af0dd47fa36070d753307cf59
Kevin <kevinlopez3292@gmail.com>
2022-11-25 07:20:34 -0500

    commit tree a910aecc67597f7a115905345154a1f6df39a76b

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+const hq = 42; //nd street
+const blockInFt = 264; //ft
+
+function distanceFromHqInBlocks(clientStreet) {
+    //returns the number of blocks given the input of client location
+    if (clientStreet > hq) {
+        const distanceInBlocks = clientStreet - hq;
+        return distanceInBlocks;
+    } else if (clientStreet < hq) {
+        const distanceInBlocks = hq - clientStreet;
+        return distanceInBlocks;
+    } else if (clientStreet == hq) {
+        return 0;
+    }
+}
+
+function distanceFromHqInFeet(clientStreet) {
+    const distInFt = (distanceFromHqInBlocks(clientStreet)) * blockInFt;
+    return distInFt;
+}
+
+function distanceTravelledInFeet(a, b) { 
+    if (a > b) {
+        const distanceTravelled = a - b;
+        const distTInFt = distanceTravelled * blockInFt;
+        return distTInFt; 
+    } else if (a < b) { 
+        const distanceTravelled = b - a;
+        const distTInFt = distanceTravelled * blockInFt;
+        return distTInFt;
+    }
+}
+
+function calculatesFarePrice(a, b) {
+    if ((distanceTravelledInFeet(a, b)) < 400) {
+        const farePrice = 0;
+        return farePrice;
+    } else if ((distanceTravelledInFeet(a, b)) < 2000 ) {
+        const farePrice = ((distanceTravelledInFeet(a, b)) - 400) * .02;
+        return farePrice;
+    } else if ((distanceTravelledInFeet(a, b)) > 2000 && (distanceTravelledInFeet(a, b)) < 2500) {
+        const farePrice = 25;
+        return farePrice;
+    } else if ((distanceTravelledInFeet(a, b)) >= 2500) {
+        const farePrice = 'cannot travel that far'
+        return farePrice;
+    }
+}
